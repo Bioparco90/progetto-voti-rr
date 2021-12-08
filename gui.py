0698b7d5a714cf4a9d2e81809fe6d1f68af917c0
Bioparco90 <77575878+Bioparco90@users.noreply.github.com>
2021-12-08 16:49:21 +0100

    create first row

--- gui.py
+++ gui.py
@@ -1,14 +1,47 @@
 from tkinter import *
 from functions import *
 
+def submit():
+    pass
+
 root = Tk()
 
 root.title("Votes Analyzer RR")
-root.geometry("600x400")
-root.resizable(False, False)
+root.geometry("800x600+0+0")
+
+# root.resizable(False, False)
+# root.grid_columnconfigure(1, weight=1)
+
 
 # Have to choose an icon, this is the facebook's icon. 
 # For path issues, just add an 'r' before "". Ex: root.iconbitmap(r"fb.ico") 
-# root.iconbitmap("fb.ico") 
+# root.iconbitmap("fb.ico")
+
+# *-------------------------------------------------------------------------------*
+# *----------------------------------HEADER---------------------------------------*
+# * welcome = Label(root, text="Votes Analyzer", font="Helvetica 20 bold", pady=5)*
+# * welcome.grid(row=0, column=2)                                                 *
+# *-------------------------------------------------------------------------------*
+
+listNameLabel = Label(root, text="List")
+listNameLabel.grid(row=1, column=0)
+
+percentageLabel = Label(root, text="%")
+percentageLabel.grid(row=1, column=1)
+
+votesLabel = Label(root, text="Votes")
+votesLabel.grid(row=1, column=2)
+
+textList = Entry(root)
+textList.grid(row=2, column=0, padx=5)
+
+textPercentage = Entry(root)
+textPercentage.grid(row=2, column=1, padx=5)
+
+textVotes = Entry(root)
+textVotes.grid(row=2, column=2, padx=5)
+
+b1 = Button(root, text="Submit", command=submit)
+b1.grid(row=2, column=3)
 
 root.mainloop()
