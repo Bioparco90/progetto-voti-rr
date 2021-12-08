829d3fe9d45102ef929a4e1a17c8f4aed0950a3e
Bioparco90 <monnimarco90@gmail.com>
2021-12-08 20:08:07 +0100

    new line button
    created a button which, when pressed, adds a new entry line

--- gui.py
+++ gui.py
@@ -1,48 +1,70 @@
 from tkinter import *
-from functions import *
+from votesFunctions import *
 
-def submit():
-    pass
+count = 1
+
+def header():
+    welcomeFrame = Frame(root)
+    header = Label(welcomeFrame, text="Votes Analyzer", font="Helvetica 20 bold", pady=5)
+    welcomeFrame.grid(row=0, column=4)
+    header.grid(row=0, column=0)
+
+
+def columnNames():
+    listNameLabel = Label(root, text="List")
+    listNameLabel.grid(row=1, column=0)
+
+    percentageLabel = Label(root, text="%")
+    percentageLabel.grid(row=1, column=1)
+
+    votesLabel = Label(root, text="Votes")
+    votesLabel.grid(row=1, column=2)
+
+
+def start():
+    textList = Entry(root, width=20, justify=CENTER)
+    textList.grid(row=2, column=0, padx=5)
+
+    textPercentage = Entry(root, width=5, justify=CENTER)
+    textPercentage.grid(row=2, column=1, padx=5)
+
+    textVotes = Entry(root, width=5, justify=CENTER)
+    textVotes.grid(row=2, column=2, padx=5)
+
+    addButton = Button(root, text="+", command=addNewList)
+    addButton.grid(row=2, column=3)
+
+
+def addNewList():
+    global count
+
+    textList = Entry(root, width=20, justify=CENTER)
+    textList.grid(row=2+count, column=0, padx=5)
+
+    textPercentage = Entry(root, width=5, justify=CENTER)
+    textPercentage.grid(row=2+count, column=1, padx=5)
+
+    textVotes = Entry(root, width=5, justify=CENTER)
+    textVotes.grid(row=2+count, column=2, padx=5)
+
+    b = Button(root, text="+", command=addNewList)
+    b.grid(row=2+count, column=3)
+    
+    count += 1
+
 
 root = Tk()
 
 root.title("Votes Analyzer RR")
 root.geometry("800x600+0+0")
 
-# root.resizable(False, False)
-# root.grid_columnconfigure(1, weight=1)
-
-
 # Have to choose an icon, this is the facebook's icon. 
 # For path issues, just add an 'r' before "". Ex: root.iconbitmap(r"fb.ico") 
 # root.iconbitmap("fb.ico")
 
-welcomeFrame = Frame(root)
-header = Label(welcomeFrame, text="Votes Analyzer", font="Helvetica 20 bold", pady=5)
-welcomeFrame.grid(row=0, column=4)
-header.grid(row=0, column=0)
 
-listNameLabel = Label(root, text="List")
-listNameLabel.grid(row=1, column=0)
-
-percentageLabel = Label(root, text="%")
-percentageLabel.grid(row=1, column=1)
-
-votesLabel = Label(root, text="Votes")
-votesLabel.grid(row=1, column=2)
-
-textList = Entry(root, width=20, justify=CENTER)
-textList.grid(row=2, column=0, padx=5)
-
-textPercentage = Entry(root, width=5, justify=CENTER)
-textPercentage.grid(row=2, column=1, padx=5)
-
-textVotes = Entry(root, width=5, justify=CENTER)
-textVotes.grid(row=2, column=2, padx=5)
-
-b1 = Button(root, text="Submit", command=submit)
-b1.grid(row=2, column=3)
-
-
+header()
+columnNames()
+start()
 
 root.mainloop()
