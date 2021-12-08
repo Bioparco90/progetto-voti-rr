9b3849f044db6d757a59d7ecf11efe6fbe194e85
Bioparco90 <monnimarco90@gmail.com>
2021-12-08 12:34:56 +0100

    create repo
    i have already done the main functions and started working on the gui

---
+++ gui.py
@@ -0,0 +1,14 @@
+from tkinter import *
+from script import *
+
+root = Tk()
+
+root.title("Votes Analyzer RR")
+root.geometry("600x400")
+root.resizable(False, False)
+
+# Have to choose an icon, this is the facebook's icon. 
+# For path issues, just add an 'r' before "". Ex: root.iconbitmap(r"fb.ico") 
+# root.iconbitmap("fb.ico") 
+
+root.mainloop()
